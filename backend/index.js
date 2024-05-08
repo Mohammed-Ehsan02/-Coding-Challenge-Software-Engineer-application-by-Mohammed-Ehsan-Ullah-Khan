ac7c4ec70fef0a315dfba7ffe61dcd22090fa0cb
Mohammed Ehsan Ullah Khan <mehsankhan02@gmail.com>
2024-05-08 13:08:45 +0400

    api for adding, removing and showing items, also added env for database connection string

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ var Express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var cors=require('cors');
 const multer = require('multer');
+require('dotenv').config();
 
 var app = Express();
 app.use(cors());
 
-var CONNECTION_STRING="";
+var CONNECTION_STRING=process.env.DB_CONNECTION;
 
 var DATABASENAME = "applcation";
 var database;
@@ -18,6 +19,31 @@ app.listen(5038, () =>{
     });
 })
 
+app.get('/backend/applcation/GetNotes', (request, response) => {
+    var collection = database.collection("applicationcollection");
+    collection.find({}).toArray((error, result) => {
+        response.send(result);
+    });
+})
+
+app.post('/backend/applcation/AddNotes', (request, response) => {
+    var collection = database.collection("applicationcollection");
+    collection.count({}, function(error, numOfDocs){
+        collection.insertOne({
+            id:(numOfDocs+1).toString(),
+            des:request.body.newNotes
+        });
+        response.json("Added Successfully");
+    });
+})
+
+app.delete('/backend/applcation/DeleteNotes', (request, response) => {
+    var collection = database.collection("applicationcollection");
+    collection.deleteOne({
+        id:request.body.id
+    });
+    response.json("Deleted Successfully");
+})
 
 
 
